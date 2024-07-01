151726e54c25bf7041d59e3afaa358a7e10da992
nicolasriverocorvalan <riveronicolas@gmail.com>
2024-07-01 10:15:06 -0300

    ethernaut

--- 26-DoubleEntryPoint/test/DoubleEntryPoint.t.sol
+++ 26-DoubleEntryPoint/test/DoubleEntryPoint.t.sol
@@ -12,9 +12,6 @@ contract DoubleEntryPointTest is Test {
     CryptoVault public cryptoVault;
     Forta public forta;
 
-    address public lgTokenAddress;
-    address public randomAddress = address(uint160(uint(keccak256(abi.encodePacked(block.timestamp, block.prevrandao)))));
-
     function setUp() public {
         legacyToken = new LegacyToken();
         legacyToken.mint(address(this), 100 ether); //my 100 ether for our wallet
@@ -34,49 +31,23 @@ contract DoubleEntryPointTest is Test {
         require(totalSupply == 100 ether, "DET total supply does not match expected value");
     }
 
-}
-
-
-
-/*
-
-
-    function testDoubleEntryPointTokenInitialSupply() public view{
-        uint256 totalSupply = doubleEntryPoint.totalSupply();
-        require(totalSupply == 100 ether, "DEP total supply does not match expected value");
-    }
-
     function testSweepTokensRecipient() public view {
         address sweptTokensRecipient = cryptoVault.sweptTokensRecipient();
         require(sweptTokensRecipient == address(this), "Swept tokens recipient does not match expected value");
     }
 
-    function testCryptoVaultAsHolderOfLegacyToken() public view {
-        uint256 balance = legacyToken.balanceOf(address(cryptoVault));
-        require(balance == 100 ether, "CryptoVault does not hold the expected amount of LET");
-    }
-
     function testSweepAfterAttack() public {        
-        cryptoVault.sweepToken(IERC20(lgTokenAddress)); // Simulate the attack
+        cryptoVault.sweepToken(IERC20(address(legacyToken))); // Simulate the attack
 
-        uint256 depTokenBalanceAfterSweep = IERC20(lgTokenAddress).balanceOf(address(cryptoVault));
-        require(depTokenBalanceAfterSweep == 0, "DEP token balance must be 0 after the attack");
-
-        (bool ok, bytes memory data) = trySweep(cryptoVault, doubleEntryPoint);
-        require(!ok, "Sweep succeeded");
-
-        // Decode the returned data to check if the DEP token balance is 0
-        // bool hasTokens = abi.decode(data, (bool));
-        // require(!hasTokens, "DEP token balance must be 0 after the attack");
+        uint256 depTokenBalanceAfterSweep = IERC20(address(legacyToken)).balanceOf(address(cryptoVault));
+        require(depTokenBalanceAfterSweep == 0, "DET token balance must be 0 after the attack");
     }
 
-    function trySweep(CryptoVault _cryptoVault, DoubleEntryPoint _instance) internal returns (bool, bytes memory) {
-        try _cryptoVault.sweepToken(IERC20(_instance.delegatedFrom())) {
-            return (true, abi.encode(false));
-        } catch {
-            return (false, abi.encode(_instance.balanceOf(_instance.cryptoVault()) > 0));
-        }
+    function testAccountTokensAfterAttack() public view {
+        uint256 detBalance = IERC20(address(doubleEntryPoint)).balanceOf(address(this));
+        uint256 letBalance = IERC20(address(legacyToken)).balanceOf(address(this));
+
+        require(detBalance == 100, "DET token balance must be 100 after the attack");
+        require(letBalance == 100, "LET token balance must be 100 after the attack");
     }
-
-
-*/
+}
